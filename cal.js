e2529471a4864bb5368fc8ddc3dd03cb008aba6d
tanyasepy@gmail.com <tanyasepy@gmail.com>
2016-03-12 14:54:06 +0700

    Create div fn

--- cal.js
+++ cal.js
@@ -13,3 +13,15 @@ function sub(x,y) {
     
     return x-y;
 }
+
+function multi(x,y) {
+     if(isNaN(x)) return console.log('Input number');
+    
+    return x*y;
+}
+
+function div(x,y) {
+     if(isNaN(x)) return console.log('Input number');
+    
+    return x/y;
+}
